11b63877b7e892f7e4322ce7f1b4a4e599b751ba
dvaumoron <dvaumoron@gmail.com>
2023-08-16 09:02:24 +0200

    comment

--- frame.go
+++ frame.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2022 puzzletest authors.
+ * Copyright 2023 puzzleframe authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
